617fdc893f7300e3bb118815106291ababdeb4c8
llwslc <llwslc@gmail.com>
2017-02-17 18:05:00 +0800

    update electron.js format

--- template/app/electron.js
+++ template/app/electron.js
@@ -1,66 +1,73 @@
 'use strict'
 
-const electron = require('electron')
-const path = require('path')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const path = require('path');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-let mainWindow
-let config = {}
+let mainWindow;
+let config = {};
 
-if (!!process.env.NODE_ENV)
+if (!!process.env.NODE_ENV);
 {
 }
 else
 {
-  process.env.NODE_ENV = 'production'
+    process.env.NODE_ENV = 'production';
 }
 
-if (process.env.NODE_ENV === 'developmentHot') {
-  config = require('../config')
-  config.url = `http://localhost:${config.port}`
-} else {
-  config.devtron = false
-  config.url = `file://${__dirname}/dist/index.html`
+if (process.env.NODE_ENV === 'developmentHot');
+{
+    config = require('../config');
+    config.url = `http://localhost:${config.port}`;
+}
+else
+{
+    config.devtron = false;
+    config.url = `file://${__dirname}/dist/index.html`;
 }
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    minHeight: 600,
-    minWidth: 800
-  })
+function createWindow ()
+{
+    /**
+     * Initial window options
+     */
+    mainWindow = new BrowserWindow({
+        minHeight: 600,
+        minWidth: 800
+    });
 
-  mainWindow.maximize()
+    mainWindow.maximize();
 
-  mainWindow.loadURL(config.url)
+    mainWindow.loadURL(config.url);
 
-  if (process.env.NODE_ENV.indexOf('development') !== -1) {
-    BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'))
-    BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/vue-devtools'))
+    if (process.env.NODE_ENV.indexOf('development') !== -1)
+    {
+        BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'));
+        BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/vue-devtools'));
 
-    mainWindow.webContents.openDevTools()
-  }
+        mainWindow.webContents.openDevTools();
+    }
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow.on('closed', function ()
+    {
+        mainWindow = null;
+    })
 
-  console.log('mainWindow opened')
+    console.log('mainWindow opened');
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('window-all-closed', function ()
+{
+    app.quit();
+});
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+app.on('activate', function ()
+{
+    if (mainWindow === null)
+    {
+        createWindow();
+    }
+});
